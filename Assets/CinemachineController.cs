9cc150107f679e7211fbf81b19889c94fa8672a3
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2020-12-28 20:50:54 +0530

    Added initial rocket take off and stage seperation mechanic.

---
+++ Assets/CinemachineController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SetPriority(int priority)
+    {
+        GetComponent<CinemachineVirtualCamera>().Priority = 1;
+    }
+}
